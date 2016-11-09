ec5c0bf9969b9b3d42c9580f3e13a0fe9cf9b19a
Niktux <nico.ln@gmail.com>
2016-11-09 02:31:55 +0100

    First steps with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,21 @@
 var gulp = require('gulp');
-gulp.task('hello', function() {
-   console.log('Hello Zell');
+var sass = require('gulp-sass');
+
+var paths = {
+    sass: 'project/assets/sass/',
+     css: 'project/assets/css/',
+     bower: 'project/vendor-front/' ,
+    www: 'project/www/'
+}
+
+gulp.task('sass', function() {
+    return gulp.src(paths.sass + 'main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.css));
+});
+
+// symlink does not work while documentation says they do !
+gulp.task('publish', function() { 
+    return gulp.src(paths.css + '*.css', {followSymlinks: false})
+        .pipe(gulp.dest(paths.www + 'assets/css')); 
 });
