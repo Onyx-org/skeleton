86a124938b70ec2c08fb2338688a7b9834e3c406
Nicolas Le Nardou <nlenardou@naoned.fr>
2016-11-21 17:03:45 +0100

    improve wizards

--- wizards.mk
+++ wizards.mk
@@ -1,11 +1,20 @@
 include .onyx
 
-wizard-set-namespace:
-	find src/ tests/ www/ -type f -exec sed -i 's/${OLD_NAMESPACE}/$(NAMESPACE)/g' {} \;
-	sed -i 's/${OLD_NAMESPACE}/$(NAMESPACE)/g' console
-	sed -i 's/${OLD_NAMESPACE}\\\\/$(NAMESPACE)\\\\/g' ./composer.json
+.PHONY: wizard-set-namespace wizard-new-controller
 
-wizard-new-controller:
+.onyx:
+	$(error .onyx file is missing)
+
+wizard-set-namespace:
+	$(eval NEW_NAMESPACE := $(shell bash -c 'read -p "Enter your application namespace : " namespace; echo $$namespace'))
+	find src/ tests/ www/ -type f -exec sed -i 's/${NAMESPACE}/$(NEW_NAMESPACE)/g' {} \;
+	sed -i 's/${NAMESPACE}/$(NEW_NAMESPACE)/g' console
+	sed -i 's/${NAMESPACE}\\\\/$(NEW_NAMESPACE)\\\\/g' ./composer.json
+	sed -i 's/^NAMESPACE=.*$$/NAMESPACE=$(NEW_NAMESPACE)/' ./.onyx
+	@echo "Namespace updated !"
+	@echo ""
+
+wizard-new-controller: .onyx
 	$(eval CONTROLLER_NAME := $(shell bash -c 'read -p "Enter your controller name : " controllerName; echo $$controllerName'))
 	$(eval TARGET_DIR := "src/Controllers/${CONTROLLER_NAME}")
 	@cp -rf wizards/controller/* src/Controllers
