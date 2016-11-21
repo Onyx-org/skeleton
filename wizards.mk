3483dba68a0192c4c6285e2367cd16b656cafd22
Niktux <nico.ln@gmail.com>
2016-11-22 00:32:02 +0100

    refactor(wizard): rename variable

--- wizards.mk
+++ wizards.mk
@@ -13,7 +13,7 @@ core-wizard-set-namespace:
 	$(info )
 	$(info Backslashes must be replaced by ::. Example : My::Onyx::Skeleton::Namespace)
 	$(info )
-	$(eval NEW_NAMESPACE := $(shell bash -c 'read -p "Enter your application namespace : " namespace; echo $$namespace'))
+	$(eval NEW_NAMESPACE := $(shell bash -c 'read -p "Enter your application newNamespace : " newNamespace; echo $$newNamespace'))
 	$(eval BACKSLASHED_NAMESPACE := $(call convert-namespace,$(NAMESPACE)))
 	$(eval BACKSLASHED_NEW_NAMESPACE := $(call convert-namespace,$(NEW_NAMESPACE)))
 	$(eval ESCAPED_NAMESPACE := $(subst \\,\\\\,$(BACKSLASHED_NAMESPACE)))
