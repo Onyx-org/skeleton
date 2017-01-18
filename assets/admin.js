aa09a12aae4b9edb9230d4a27533a9e70f4c7bdf
Skimi <rgebski@naoned.fr>
2017-01-18 14:07:07 +0100

    fix(webpack): Added missing required polyfills

--- assets/admin.js
+++ assets/admin.js
@@ -1,3 +1,6 @@
-require('bootstrap');
+import 'core-js/es6/promise';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'bootstrap';
 
 console.log('admin');
--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,6 @@
+import 'core-js/es6/promise';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
 import 'bootstrap';
 import 'sass/vendor.scss';
 import 'sass/main.scss';
