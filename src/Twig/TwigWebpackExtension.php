4dc9e27b0bd29934465c5da45f44a129657ad03c
Skimi <rgebski@naoned.fr>
2017-01-27 12:08:56 +0100

    refact(TwigWebpackExtension): DRYer code & code style

--- src/Twig/TwigWebpackExtension.php
+++ src/Twig/TwigWebpackExtension.php
@@ -6,19 +6,22 @@ use __ONYX_Namespace\Webpack\WebpackManifest;
 
 class TwigWebpackExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
 {
+    private
+        $manifest;
+
     public function __construct(WebpackManifest $manifest)
     {
         $this->manifest = $manifest;
     }
 
-    public function getGlobals()
+    public function getGlobals(): array
     {
         return array(
             'webpackChunkManifest' => $this->manifest->getChunkManifest(),
         );
     }
 
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new \Twig_SimpleFunction('webpackAssets', [$this, 'webpackAssets']),
@@ -28,34 +31,42 @@ class TwigWebpackExtension extends \Twig_Extension implements \Twig_Extension_Gl
 
     /**
      * Outputs list of files from the webpack compilation manifest
-     * @param  $string $include A glob expression of files to include
-     * @param  $string $include A glob expression of files to exclude
-     * @return Array            List of file paths
+     * @param  $string $includePattern A glob expression of files to include
+     * @param  $string $excludePattern A glob expression of files to exclude
+     * @return Array                   List of file paths
      */
-    public function webpackAssets($include = null, $exclude = null)
+    public function webpackAssets(?string $includePattern = null, ?string $excludePattern = null): array
     {
         $validFiles = $this->manifest->getFiles();
 
-        if (!empty($include)) {
-            $validFiles = [];
-            $includePatterns = explode(',', $include);
-            foreach ($includePatterns as $pattern) {
-                $validFiles += array_filter($this->manifest->getFiles(), function($fileName) use ($pattern) {
-                    return fnmatch(trim($pattern), $fileName, FNM_CASEFOLD);
-                }, ARRAY_FILTER_USE_KEY);
-            }
+        if (!empty($includePattern))
+        {
+            $validFiles = $this->filterAssets($includePattern);
         }
 
-        if (!empty($exclude)) {
-            $excludePatterns = explode(',', $exclude);
-            foreach ($excludePatterns as $pattern) {
-                $validFiles = array_filter($validFiles, function($fileName) use ($pattern) {
-                    return !fnmatch(trim($pattern), $fileName);
-                }, ARRAY_FILTER_USE_KEY);
-            }
+        if (!empty($excludePattern))
+        {
+            $invalidFiles = $this->filterAssets($excludePattern);
+            $validFiles = array_diff($validFiles, $invalidFiles);
         }
 
         return $validFiles;
+    }
+
+    private function filterAssets(string $includeGlob): array
+    {
+        $assets = $this->manifest->getFiles();
+        $filteredAssets = [];
+        $includePatterns = explode(',', $includeGlob);
+
+        foreach ($includePatterns as $globPattern)
+        {
+            $filteredAssets += array_filter($assets, function($fileName) use ($globPattern) {
+                return fnmatch(trim($globPattern), $fileName, FNM_CASEFOLD);
+            }, ARRAY_FILTER_USE_KEY);
+        }
+
+        return array_unique($filteredAssets);
     }
 
     /**
@@ -63,7 +74,7 @@ class TwigWebpackExtension extends \Twig_Extension implements \Twig_Extension_Gl
      * @param  string $name Exact name of the file
      * @return string       Path to the file
      */
-    public function webpackAsset($name)
+    public function webpackAsset($name): ?string
     {
         return $this->manifest->getFiles()[$name];
     }
