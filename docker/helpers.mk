1ab3f83385c6082a0d21d6bcc352e0c1ef725776
Claude LE BRIS <claude.lebris@gmail.com>
2017-02-27 15:54:42 +0100

    fix: clean docker volumes when removing containers

--- docker/helpers.mk
+++ docker/helpers.mk
@@ -12,7 +12,7 @@ build:
 rebuild: build up
 
 down:
-	docker-compose -f docker/docker-compose.yml down
+	docker-compose -f docker/docker-compose.yml down --volumes
 
 connect:
 	docker exec --tty -i onyx-frontend /bin/bash
