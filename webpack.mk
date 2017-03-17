6fc83fed109fd274d60bc96819ec6dc3601d6daf
Niktux <nico.ln@gmail.com>
2017-03-17 23:29:04 +0100

    fix(webpack): .npm issue with user =/= 1000
    closes #19

--- webpack.mk
+++ webpack.mk
@@ -1,5 +1,6 @@
 npm = docker run -it --rm \
 		-v ${ONYX_DIR}:/usr/src/app \
+		-v ${HOME}/.npm:/.npm \
 		-u ${USER_ID}:${GROUP_ID} \
 		-w /usr/src/app \
 		${2} \
@@ -8,6 +9,7 @@ npm = docker run -it --rm \
 
 npm2 = docker run -it --rm \
 		-v ${ONYX_DIR}:/usr/src/app \
+		-v ${HOME}/.npm:/.npm \
 		-u ${USER_ID}:${GROUP_ID} \
 		-w /usr/src/app \
 		${2} \
@@ -17,8 +19,11 @@ npm2 = docker run -it --rm \
 bash:
 	$(call npm2, bash)
 
-npm-install:
+npm-install: npm-cache-dir
 	$(call npm, install)
+
+npm-cache-dir:
+	mkdir -p ~/.npm
 
 webpack: clean-webpack-built-assets
 	$(call npm, run build)
@@ -29,4 +34,4 @@ webpack-prod: clean-webpack-built-assets
 clean-webpack-built-assets:
 	rm -rf www/assets
 
-.PHONY: npm webpack webpack-dev webpack-watch clean-webpack-built-assets
+.PHONY: npm webpack webpack-dev webpack-watch clean-webpack-built-assets npm-cache-dir
