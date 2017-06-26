d51d2cd4b684190fe903a2f4fcc359967d749ecc
Nicolas Le Nardou <nlenardou@naoned.fr>
2017-06-26 15:52:03 +0200

    refactor: route lister command

--- src/Console.php
+++ src/Console.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace __ONYX_Namespace;
 
 use Puzzle\Configuration;
 use Pimple\Container;
+use Onyx\Services\Routes\Retrievers\Silex;
+use Onyx\Console\Commands\RouteLister;
 
 class Console
 {
@@ -17,34 +21,12 @@ class Console
 
         $this->app = new \Onyx\Console\Application();
 
-        $this->registerRouteLister($container);
-
+        $this->app->add(new RouteLister(new Silex($container)));
         $this->app->add(new Console\HelloWorld());
     }
 
     public function run(): void
     {
         $this->app->run();
-    }
-
-    private function registerRouteLister(Container $container)
-    {
-        $this->app->add(new \Onyx\Console\Commands\RouteLister(new class($container) implements \Onyx\Console\Commands\Routes\Retriever{
-                private
-                    $app;
-
-                public function __construct(Container $app)
-                {
-                    $this->app = $app;
-                }
-
-                public function retrieveRoutes(): iterable
-                {
-                    $this->app->flush();
-
-                    return $this->app['routes'];
-                }
-            }
-        ));
     }
 }
