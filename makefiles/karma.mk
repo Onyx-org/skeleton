489f6ebcbbfdad30bf683500d4b2ebd53c05b5d4
Claude LE BRIS <claude.lebris@gmail.com>
2017-10-11 22:32:16 +0200

    chore(makefile): allow karma env to be given as parameter or env var

--- makefiles/karma.mk
+++ makefiles/karma.mk
@@ -2,8 +2,21 @@
 # Karma
 #------------------------------------------------------------------------------
 
+# Spread cli arguments
+ifneq (,$(filter $(firstword $(MAKECMDGOALS)),config))
+    KARMA_CLI_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+    $(eval $(KARMA_CLI_ARGS):;@:)
+endif
+
+ifeq (,$(KARMA_CLI_ARGS))
+	KARMA_CLI_ARGS=$(shell grep 'KARMA_ENV=' .env | sed 's/KARMA_ENV=//g')
+	ifeq (,$(KARMA_CLI_ARGS))
+		KARMA_CLI_ARGS=dev
+	endif
+endif
+
 config: karma ## Run karma to configure for development environment
-	./karma hydrate -e dev
+	./karma hydrate -e $(KARMA_CLI_ARGS)
 
 karma:
 	$(eval LATEST_VERSION := $(shell curl -L -s -H 'Accept: application/json' https://github.com/niktux/karma/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/'))
